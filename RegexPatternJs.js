18becaf4ea982da5f1847f06f4d5a1779e4d18a9
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-07 09:36:27 +0530

    Added regex pattern for email to validate UC2 of email

--- RegexPatternJs.js
+++ RegexPatternJs.js
@@ -6,7 +6,7 @@ let zip2 = "A400088";// UC2 - Regex pattern to validate zip code A400088 should 
 let zip3 = "400088B";// UC3 - Regex pattern to validate zip code 400088B should fail
 let zip4 = "400 088";// UC4 - Regex pattern to validate zip code 400 088 should pass
 let email1 = "abc.xyz@bridgelabz.co.in";// UC1 - Regex pattern to validate email to check abc part and should pass
-
+let email2 = "abc.xyz@bridgelabz.co.in";// UC2 - Regex pattern to validate email to check @ part and should pass
 try{
     if(!ZIPCODE.test(zip4)) throw "Invalid Zip code"
     {
@@ -16,10 +16,11 @@ try{
     console.log(Exception);
 }
 try{
-    if(!EMAIL.test(email1)) throw "Invalid emailId"
+    if(!EMAIL.test(email2)) throw "Invalid emailId"
     {
         console.log("Valid emailId");
     }
 }catch(Exception){
     console.log(Exception);
 }
+
