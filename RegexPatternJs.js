3734d19c3c9f591507be4c27c430e2c645ae152b
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-07 10:30:30 +0530

    Added regex pattern for zip code to validate UC3

--- RegexPatternJs.js
+++ RegexPatternJs.js
@@ -2,8 +2,9 @@ const prompt = require('prompt-sync')();
 const ZIPCODE = RegExp("^[0-9]{6}$");
 let zip1 = "400088";// UC1 - Regex pattern to validate zip code 400088 should pass
 let zip2 = "A400088";// UC2 - Regex pattern to validate zip code A400088 should fail
+let zip3 = "400088B";// UC3 - Regex pattern to validate zip code 400088B should fail
 try{
-    if(!ZIPCODE.test(zip2)) throw "Invalid Zip code"
+    if(!ZIPCODE.test(zip3)) throw "Invalid Zip code"
     {
         console.log("Valid Zip code");
     }
